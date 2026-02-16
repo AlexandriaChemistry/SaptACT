141be2950fceaed6cf63e469799b42cf3ffdca33
David van der Spoel <david.vanderspoel@gmail.com>
2026-02-16 12:35:25 +0100

    Updated scripts

--- psi4files.py
+++ psi4files.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 import glob, json, os, sys, math
-import openbabel as ob
 import molprops
 import get_mol_dict       as gmd
-from gaff_to_alexandria       import *
+from atomic_heat_of_formation import *
 from run_calcs  import special_basis
 from elements  import *
 from enum import Enum
@@ -21,6 +20,9 @@ class Psi4Error(Enum):
     LargeDistance = 7
     MonomerCharge = 8
     InconsistentJson = 9
+    ShortDistance = 10
+    InconsistentNumberOfAtoms = 11
+    Unknown = 12
 
 def psi4msg(p4:Psi4Error):
     msgs = {
@@ -33,7 +35,10 @@ def psi4msg(p4:Psi4Error):
         Psi4Error.Coordinates: "Psi4Error: Coordinates incorrect",
         Psi4Error.HighEnergy: "Psi4Error: Energy too high",
         Psi4Error.LargeDistance: "Psi4Error: Distance too large",
-        Psi4Error.MonomerCharge: "Psi4Error: Compound missing in monomer_charge.csv"
+        Psi4Error.ShortDistance: "Psi4Error: Distance too short",
+        Psi4Error.MonomerCharge: "Psi4Error: Compound missing in monomer_charge.csv",
+        Psi4Error.InconsistentNumberOfAtoms: "Psi4Error: Inconsistent number of atoms",
+        Psi4Error.Unknown: "Psi4Error: unknown"
     }
     return msgs[p4]
 
@@ -55,6 +60,7 @@ def check_dist(allcoords:list)->float:
         return 0
 
 verbose              = False
+debug                = False
 hasPrintedIncomplete = False
 edimerMinDefault     = 1e8
 class Psi4Files:
@@ -83,10 +89,56 @@ class Psi4Files:
             self.jobtype = myjson[jt]
         return myjson
 
-    def read(self, args, g2a,
+    def add_props(self, myexp, mydict, temperature:float, logf):
+        # Add electrostatic stuff
+        mytype  = "Electronic"
+        if "alpha" in mydict:
+            alp     = mydict["alpha"]
+            if None == alp:
+                logf.write("Empty alpha in %s/%s\n" % ( os.getcwd(), self.dataFile ))
+            else:
+                if verbose:
+                    logf.write("In %s\n" % os.getcwd())
+                    logf.write("Found alpha {}\n".format(alp))
+                average = str((alp["XX"]+alp["YY"]+alp["ZZ"])/3.0)
+                myexp.add_polarisability(mytype, "Angstrom3", temperature, average, 0,
+                                         str(alp["XX"]), str(alp["YY"]), str(alp["ZZ"]),
+                                         str(alp["XY"]), str(alp["XY"]), str(alp["YZ"]))
+        if "Dipole" in mydict:
+            dip = mydict["Dipole"]
+            if None == dip:
+                logf.write("Empty dipole in %s/%s\n" % ( os.getcwd(), self.dataFile ))
+            else:
+                average = str(math.sqrt(dip["X"]**2 + dip["Y"]**2 + dip["Z"]**2))
+                myexp.add_dipole(mytype, dip["unit"], temperature, average, 0,
+                                 str(dip["X"]), str(dip["Y"]), str(dip["Z"]))
+        if "Quadrupole" in mydict:
+            quad = mydict["Quadrupole"]
+            if None == quad:
+                logf.write("Empty quadrupole in %s/%s\n" % ( os.getcwd(), self.dataFile ))
+            else:
+                quad_trace = (quad["XX"]+quad["YY"]+quad["ZZ"])/3.0
+                myexp.add_quadrupole(mytype, "B", temperature,
+                                     str(quad["XX"]-quad_trace), str(quad["YY"]-quad_trace), str(quad["ZZ"]-quad_trace), 0, 0, 0)
+        freq = "frequencies"
+        inten = "intensities"
+        if freq in mydict and inten in mydict:
+            for f in mydict[freq]:
+                myexp.frequencies.append(str(f))
+            for i in mydict[inten]:
+                myexp.intensities.append(str(i))
+        # Special stuff
+        mytemp  = 298.15
+        myphase = "gas"
+        if "entropy" in mydict:
+            myexp.add_cv_entropy(mydict["entropy"], mytemp, myphase)
+        if "Cv" in mydict:
+            myexp.add_cv_entropy(mydict["Cv"], mytemp, myphase)
+        
+    def read(self, args,
              molnames:list, monoq:dict, calc:str, mycomplex:str, filename:str,
-             Mols:Molecules,
-             temperature:float, logf)->Psi4Error:
+             Mols:Molecules, ahof,
+             temperature:float, potential:list, logf)->Psi4Error:
         global hasPrintedIncomplete
         self.status = Psi4Error.OK
         nfrag = len(mycomplex)
@@ -113,16 +165,34 @@ class Psi4Files:
         fAbsMax = 0
         self.lots      = []
         self.charges   = []
+        # Check whether there are forces in json
+        useForces = True
+        for k in range(len(mydict[mols])):
+            nForces = 0
+            if atoms in mydict[mols][k]:
+                for j in range(len(mydict[mols][k][atoms])):
+                    if "forces" in mydict[mols][k][atoms][j]:
+                        nForces += 1
+                useForces = useForces and (nForces == len(mydict[mols][k][atoms]))
+            else:
+                useForces = False
+        if verbose:
+            logf.write("Found forces in %s: %s\n" % ( os.getcwd(), useForces ) )
         # Create new experiment
-        myexp = molprops.Experiment("Theory", "Spoel2025a", "Psi4", args.method, args.basis,
-                                    "conformation", self.jobtype, filename, True)
+        myexp = molprops.Experiment("Theory", args.reference, "Psi4", args.method, args.basis,
+                                    "conformation", self.jobtype, filename, useForces)
+        # Add molecular properties
+        self.add_props(myexp, mydict, temperature, logf)
+        # Atoms etc.
         frag_charges = []
         sum_q_abs    = 0
+        positive     = 1
         # TODO Fix charges!
         for j in range(self.nmols):
             if molnames[j] in monoq:
                 mq = monoq[molnames[j]]["charge"]
                 frag_charges.append(mq)
+                positive = positive*mq
                 sum_q_abs += abs(mq)
             else:
                 self.status = Psi4Error.MonomerCharge
@@ -137,6 +207,9 @@ class Psi4Files:
             coords   = []
             forces   = []
             myatoms  = []
+            if not atoms in mydict[mols][k]:
+                logf.write("Incomplete json in %s/%s\n" % ( os.getcwd(), self.dataFile) )
+                continue
             natom    = len(mydict[mols][k][atoms])
             for j in range(natom):
                 elements.append(mydict[mols][k][atoms][j]["elem"])
@@ -156,39 +229,50 @@ class Psi4Files:
                 self.lots.append(lot)
                 # Atom counter for fragments
                 myatoms.append(offset+j+1)
+                # These are atomic charges for use in heat of formation calcs
                 self.charges.append(0)
-            if k >= len(self.MD):
-                sys.exit("k = %d len(self.MD) = %d" % ( k, len(self.MD)))
             if (self.status == Psi4Error.OK and
-                not self.MD[k].from_coords_elements(elements[offset:], coords, charge=frag_charges[k])):
-                logf.write("Cannot analyze coordinates using OpenBabel\n")
+                not self.MD[k].from_coords_elements(molnames[k], elements[offset:], coords, frag_charges[k])):
+                logf.write("Cannot analyze coordinates using RDKit\n")
                 self.status = Psi4Error.Coordinates
+            if debug:
+                logf.write("1. There are %d atoms for %s k = %d\n" % ( len(self.MD[k].atoms), molnames[k], k ) )
             if self.status == Psi4Error.OK and not self.firstDone:
                 symmetry = 1
                 mult     = 1
-                mol      = Mols.find_mol(molnames[k])
-                if mol:
-                    symmetry = mol.symmetry_number
-                    mult     = mol.mult
-                frag     = molprops.Fragment(self.MD[k].inchi, frag_charges[k], mult, symmetry,
-                                             myatoms, self.MD[k].mol_weight, self.MD[k].formula)
-                self.mp1.add_fragment(frag)
-                for b in self.MD[k].bonds:
-                    self.mp1.add_bond(offset+b[0], offset+b[1], self.MD[k].bonds[b])
-            if self.status == Psi4Error.OK:
-                # Now add the atoms to the experiment
-                if 0 == len(self.MD[k].atoms):
-                    sys.exit("No atoms in compound %d" % k )
-                for atom in range(natom):
-                    obtype = g2a.rename(self.MD[k].atoms[1+atom]["obtype"])
-                    # Atom numbers should match bonds
-                    myexp.add_atom(elements[offset+atom], obtype, offset+atom+1,
+                mymol    = Mols.find_mol(molnames[k])
+                if mymol:
+                    symmetry = mymol.symmetry_number
+                    mult     = mymol.mult
+                    if mymol.stdinchi != self.MD[k].inchi:
+                        logf.write("Warning: overriding generated InChi (%s) for %s with database InChi (%s)\n" % ( self.MD[k].inchi, molnames[k], mymol.stdinchi ) )
+                        self.MD[k].inchi = mymol.stdinchi
+                frag = molprops.Fragment(self.MD[k].inchi, frag_charges[k], mult, symmetry,
+                                         myatoms, self.MD[k].mol_weight, self.MD[k].formula)
+                if len(self.mp1.fragments) > 1:
+                    logf.write("There are already %d fragments, skipping this one\n" % len(self.mp1.fragments))
+                else:
+                    self.mp1.add_fragment(frag)
+                    for b in self.MD[k].bonds:
+                        self.mp1.add_bond(offset+b[0], offset+b[1], self.MD[k].bonds[b])
+            if debug:
+                logf.write("2. There are %d atoms for %s k = %d\n" % ( len(self.MD[k].atoms), molnames[k], k ) )
+            # Now add the atoms to the experiment
+            for atom in range(len(self.MD[k].atoms)):
+                obtype = self.MD[k].atoms[atom]["obtype"]
+                # Atom numbers should match bonds
+                if offset + atom >= len(elements):
+                    logf.write(f"Inconsistent number of atoms for {molnames[k]}. There are {len(elements)} elements, offset = {offset}, natom = {natom}. len(self.MD[{k}].atoms) = {len(self.MD[k].atoms)}\n" )
+                    self.status = Psi4Error.InconsistentNumberOfAtoms
+                else:
+                    # Note that atom numbers from zero, but MolDict from 1
+                    myexp.add_atom(elements[offset+atom], obtype, offset+atom,
                                    "Angstrom",
                                    coords[atom][0], coords[atom][1], coords[atom][2],
                                    "Hartree/Bohr",
                                    forces[atom][0], forces[atom][1], forces[atom][2])
-                offset   += natom
-                allcoords.append(coords)
+            offset   += natom
+            allcoords.append(coords)
         # This has to come after the atoms because we need the elements here
         if not energies in mydict:
             self.status = Psi4Error.IncompleteJson
@@ -196,58 +280,68 @@ class Psi4Files:
         else:
             if len(molnames) == 2:
                 # We have a dimer
+                deltaEmax = args.deltaEmax*(1+sum_q_abs)
+                exch      = "Exchange"
+                intener   = "InteractionEnergy"
                 if self.edimerMin == edimerMinDefault:
                     logf.write("edimerMin has not been set\n")
                     self.status = Psi4Error.IncompleteJson
-                exch    = "Exchange"
-                intener = "InteractionEnergy"
-
-                if self.edimerMin == edimerMinDefault:
-                    logf.write("In %s\n" % os.getcwd())
-                    logf.write(f"Minimum energy not available for {molnames[0]}-{molnames[1]}. Considering exchange energy.\n")
-                    
-                    if exch in mydict[energies]:
-                        if mydict[energies][exch] > args.deltaEmax * (1 + sum_q_abs):
-                            logf.write(f"Skipping high {exch} energy {mydict[energies][exch]} for {molnames[0]}-{molnames[1]}\n")
-                            self.status = Psi4Error.HighEnergy
-                        else:
-                            logf.write(f"Processing but ignoring exchange energy for {molnames[0]}-{molnames[1]}\n")
-                    else:
-                        logf.write(f"No exchange energy available for {molnames[0]}-{molnames[1]}\n")
-                        self.status = Psi4Error.MissingEnergy
-
-                if exch in mydict[energies]:
-                    if mydict[energies][exch] > args.deltaEmax*(1+sum_q_abs):
-                        logf.write("Skipping high %s energy %g for %s-%s\n" %
-                                   ( exch, mydict[energies][exch], molnames[0], molnames[1] ))
-                        self.status = Psi4Error.HighEnergy
-                elif intener in mydict[energies]:
-                    if (mydict[energies][intener] - self.edimerMin) > args.deltaEmax*(1+sum_q_abs):
-                        logf.write("Skipping high %s energy %g for %s-%s\n" %
-                                   ( intener, mydict[energies][intener], molnames[0], molnames[1] ))
-                        self.status = Psi4Error.HighEnergy
-                if args.deltaHF:
-                    dhf = "delta HF,r (2)"
-                    mp2 = "delta MP2,r (2)"
-                    induc = "Induction"
-                    if (dhf in mydict[energies] and
-                        induc in mydict[energies]):
-                        indcorr = mydict[energies][dhf]
-                        if mp2 in mydict[energies]:
-                            indcorr += mydict[energies][mp2]
-                        mydict[energies][induc] -= indcorr
-                        mydict[energies]["InductionCorrection"] = indcorr
-                
                 if self.status == Psi4Error.OK:
+                    if positive > 0:
+                        # Compounds with equal signed charge
+                        if exch in mydict[energies]:
+                            if mydict[energies][exch] > deltaEmax:
+                                logf.write(f"Skipping high {exch} energy {mydict[energies][exch]} for {molnames[0]}-{molnames[1]}\n")
+                                self.status = Psi4Error.HighEnergy
+                        else:
+                            logf.write(f"No exchange energy available for {molnames[0]}-{molnames[1]}\n")
+                            self.status = Psi4Error.IncompleteJson
+
+                    else:
+                        if exch in mydict[energies]:
+                            if mydict[energies][exch] > deltaEmax:
+                                logf.write(f"Skipping high {exch} energy {mydict[energies][exch]} for {molnames[0]}-{molnames[1]}\n")
+                                self.status = Psi4Error.HighEnergy
+                        else:
+                            logf.write(f"No exchange energy available for {molnames[0]}-{molnames[1]}\n")
+                            self.status = Psi4Error.IncompleteJson
+#                        if (mydict[energies][intener] - self.edimerMin) > deltaEmax:
+#                            logf.write("Skipping high %s energy %g for %s-%s\n" %
+#                                       ( intener, mydict[energies][intener], molnames[0], molnames[1] ))
+#                            self.status = Psi4Error.HighEnergy
+
+                if self.status == Psi4Error.OK:
+                    if args.deltaHF:
+                        dhf = "delta HF,r (2)"
+                        mp2 = "delta MP2,r (2)"
+                        induc = "Induction"
+                        if (dhf in mydict[energies] and
+                            induc in mydict[energies]):
+                            indcorr = mydict[energies][dhf]
+                            if mp2 in mydict[energies]:
+                                indcorr += mydict[energies][mp2]
+                            mydict[energies][induc] -= indcorr
+                            mydict[energies]["InductionCorrection"] = indcorr
+                
                     for energy in mydict[energies].keys():
                         myexp.add_energy(energy, "Hartree", 0, "gas", mydict[energies][energy])
                 else:
                     logf.write("Skipping energies in %s because of %s" %
                                ( os.getcwd(), psi4msg(self.status) ) )
             else:
-                self.status = Psi4Error.InconsistentJson
-            
-        
+                # Monomers
+                unit = "Hartree"
+                if "unit" in mydict[energies]:
+                    unit = mydict[energies]["unit"]
+                deltaE0 = compute_dhform(mydict[energies]["energy"],
+                                         elements, ahof,
+                                         self.lots, self.charges, temperature)
+                myexp.add_energy("DeltaE0", unit, 0, "gas", deltaE0)
+                # Zero point vibrational energy
+                ZPVE = "ZPVE"
+                if ZPVE in mydict[energies]:
+                    myexp.add_energy("ZPE", unit, 0, "gas", mydict[energies][ZPVE])
+
         if self.status == Psi4Error.OK:
             if self.nmols == 2:
                 mydist = check_dist(allcoords)
@@ -255,10 +349,24 @@ class Psi4Files:
                     self.status = Psi4Error.LargeDistance
                     logf.write("Distance (%g) too large (max %g) between compounds in %s\n" %
                                ( mydist, args.rmax, os.getcwd() ) )
+                elif mydist < args.rmin:
+                    self.status = Psi4Error.ShortDistance
+                    logf.write("Distance (%g) too short (max %g) between compounds in %s\n" %
+                               ( mydist, args.rmin, os.getcwd() ) )
+        if self.status ==  Psi4Error.OK and len(potential) > 0:
+            self.add_potential(potential, myexp)
         if self.status == Psi4Error.OK:
             self.mp1.add_experiment(myexp)
+        #if self.status == Psi4Error.OK:
         self.firstDone = True
         return self.status
+
+    def add_potential(self, potential:list, myexp:molprops.Experiment):
+        for np in range(len(potential)):
+            p = potential[np]
+            myexp.add_potential(str(np), "Hartree/e", "pm",
+                                100*p["grid"][0], 100*p["grid"][1], 100*p["grid"][2],
+                                p["value"])
 
     def molprop(self)->molprops.Molprop:
         return self.mp1
